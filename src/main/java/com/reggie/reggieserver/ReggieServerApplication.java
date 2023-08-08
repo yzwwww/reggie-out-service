cc777c42bcc7a7502fd93f103a6ca26276bdeb0c
yzwwww <2847469631@qq.com>
2023-08-08 12:58:30 +0800

    init

---
+++ src/main/java/com/reggie/reggieserver/ReggieServerApplication.java
@@ -0,0 +1,13 @@
+package com.reggie.reggieserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReggieServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reggie/reggieserver/ReggieServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reggie.reggieserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReggieServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
